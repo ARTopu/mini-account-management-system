26032f37e7bd625093a43cd69031b1892bda5897
Topu <atiqurrahman2312002@gmail.com>
2025-06-03 21:10:39 +0600

    Initial project setup with ASP.NET Core Razor Pages, Identity, SQL Server connection, and role seeding

---
+++ AccountManagementSystem/Data/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public class SeedData
+{
+    public static async Task SeedRoles(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] roleNames = { "Admin", "Accountant", "Viewer" };
+
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+    }
+}
